815326b8e77b559dc24d520f667a1f04388931c6
caliphsafe <84346820+caliphsafe@users.noreply.github.com>
2025-03-28 21:39:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -61,9 +61,19 @@ Wave that Cooper Flagg nigga
 
 // 3) REPLACE PARENTHESES WITH INPUT BOXES
 const lyricsElement = document.getElementById("lyrics");
+
+let clueCounter = 0; // track clue number
+
 const lyricsWithInputs = originalLyrics.replace(/\((.*?)\)/g, (match, p1) => {
-  return `<input type="text" class="input-box" data-answer="${p1.toUpperCase()}" maxlength="${p1.length}" />`;
+  clueCounter++;
+  return `
+    <span class="input-wrapper">
+      <span class="clue-number">${clueCounter}</span>
+      <input type="text" class="input-box" data-answer="${p1.toUpperCase()}" maxlength="${p1.length}" />
+    </span>`;
 });
+
+
 // Display the lyrics with input boxes
 lyricsElement.innerHTML = lyricsWithInputs;
 
